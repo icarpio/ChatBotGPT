190579110e42be33d678362365f4f54e73c8b059
Ivan <icarpiodeveloper@gmail.com>
2024-06-19 20:32:05 +0200

    Solved R problems

--- app/src/main/java/com/example/chatbotgpt/MainActivity.kt
+++ app/src/main/java/com/example/chatbotgpt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.chatbotgpt
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatbotgpt.repository.ChatRepository
+import com.example.chatbotgtp.R
 
 class MainActivity : AppCompatActivity() {
 
